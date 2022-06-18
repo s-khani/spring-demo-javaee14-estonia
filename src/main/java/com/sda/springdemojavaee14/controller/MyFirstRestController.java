75d62ba44b0a0505f46e31152857aabd7fe049af
maniek <mpastuszka@gmail.com>
2022-06-18 14:00:41 +0300

    [change] - adding some comments for code and first service created

--- src/main/java/com/sda/springdemojavaee14/controller/MyFirstRestController.java
+++ src/main/java/com/sda/springdemojavaee14/controller/MyFirstRestController.java
@@ -3,13 +3,20 @@ package com.sda.springdemojavaee14.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//@RestController creates now Spring bean based on the class
+//bean = component = managed object (object created by Spring for us)
+//TOO:install postman
 @RestController
 public class MyFirstRestController {
 
     private static final Logger log = LoggerFactory.getLogger(MyFirstRestController.class);
 
+   //@GetMapping means
+    // - code of the method will be run when we hit endpoint
+    //we use exact HTTP method
     @GetMapping("/say-hello")
     public String hello() {
         // TODO: explain logger level
---
+++ src/main/java/com/sda/springdemojavaee14/service/MyFirstService.java
@@ -0,0 +1,7 @@
+package com.sda.springdemojavaee14.service;
+
+import org.springframework.stereotype.Service;
+
+@Service //@Service- for business logic
+public class MyFirstService {
+}
