f9409d4d8798c46cc1e96f83c0637f4436c01591
maniek <mpastuszka@gmail.com>
2022-06-19 11:37:07 +0300

    [add] creating GreetingService

---
+++ src/main/java/com/sda/springdemojavaee14/service/GreetingService.java
@@ -0,0 +1,17 @@
+package com.sda.springdemojavaee14.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j // provide lof field for us
+//as the same as private static final Logger log = LoggerFactory.getLogger(GreetingService.class);
+public class GreetingService {
+    public String makeSomeGreetingToUser(String name){
+
+        String result = "Welcome to Sprig class" + name;
+        log.info(result);
+        return result;
+
+    }
+}
