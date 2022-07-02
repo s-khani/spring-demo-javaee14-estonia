f48728dc8b85252e141d67ca6f787662dc02c2c8
“Samaneh” <s.khanimirzaee@gmail.com>
2022-07-02 11:10:48 +0300

    [change]- using @Scope with prototype to provide new wheel everytime it's needed

--- src/main/java/com/sda/springdemojavaee14/homework/ex1/Wheel.java
+++ src/main/java/com/sda/springdemojavaee14/homework/ex1/Wheel.java
@@ -1,8 +1,13 @@
 package com.sda.springdemojavaee14.homework.ex1;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Component
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+
+
+@Component("ratas")
+@Scope(SCOPE_PROTOTYPE)
 public class Wheel {
 
     public Wheel() {
