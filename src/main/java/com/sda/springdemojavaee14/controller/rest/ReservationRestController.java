d8ccdc769cbfefaed11b81a6e77023b3934f0afd
“Samaneh” <s.khanimirzaee@gmail.com>
2022-07-09 10:54:49 +0300

    [change]-moving dummy data from ReservationRestController to ReservationService

--- src/main/java/com/sda/springdemojavaee14/controller/rest/ReservationRestController.java
+++ src/main/java/com/sda/springdemojavaee14/controller/rest/ReservationRestController.java
@@ -2,8 +2,8 @@ package com.sda.springdemojavaee14.controller.rest;
 
 
 import com.sda.springdemojavaee14.entity.Reservation;
+import com.sda.springdemojavaee14.service.ReservationService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.LifecycleState;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,27 +16,18 @@ import java.util.List;
 @RequestMapping("/api")
 public class ReservationRestController {
 
+    private final ReservationService reservationService;
+
+    public ReservationRestController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
 
     @GetMapping("/reservations")
     public List<Reservation> getAllReservation(){
 
         log.info("getting all Reservations");
-        return List.of(
-                new Reservation(1L, 12L, LocalDateTime.now(), LocalDateTime.now().plusHours(2),
-                        "Wołek", "51124", "m@op.pl", 10, ""),
-
-                Reservation.builder()
-                        .id(1L)
-                        .tableNumber(12L)
-                        .startBookingTime(LocalDateTime.now())
-                        .endBookingTime(LocalDateTime.now().plusHours(2))
-                        .surname("Samaneh")
-                        .phoneNumber("555 555")
-                        .email("s.khanimirzaee@gmail.com")
-                        .numberOfPeople(5)
-                        .address("address 5")
-                        .build()
-        );
+        return reservationService.findAllReservation();
 
     }
 }
