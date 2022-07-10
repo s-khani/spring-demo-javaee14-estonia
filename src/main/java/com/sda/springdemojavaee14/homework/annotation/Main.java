a9d44a898b0d353ba5655077325cc0672e6332d8
“Samaneh” <s.khanimirzaee@gmail.com>
2022-07-10 11:16:37 +0300

    [add]- Person class with builder

--- src/main/java/com/sda/springdemojavaee14/homework/annotation/Main.java
+++ src/main/java/com/sda/springdemojavaee14/homework/annotation/Main.java
@@ -18,7 +18,16 @@ public class Main {
         System.out.println("is this the same person: " + me.equals(anotherMe));
 
         System.out.println("are equals: " + me.equals("haha"));
+
+        Person meAgain = new Person("khani","Samaneh",31);
+        //builder is better than all args constructor - because you can see more :)
+        Person meAgainWithBuilder = Person.builder()
+                .name("Samaneh")
+                .surname("Khani")
+                .age(31)
+                .build();
     }
+
 
 
 
