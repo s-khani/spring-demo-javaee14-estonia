8be0677922bf864576cd03f0d16c1313b616ff5e
“Samaneh” <s.khanimirzaee@gmail.com>
2022-07-10 11:09:49 +0300

    [add]- Person class with lambok annotations which mirrors PersonWithAllSourceCode

---
+++ src/main/java/com/sda/springdemojavaee14/homework/annotation/Person.java
@@ -0,0 +1,16 @@
+package com.sda.springdemojavaee14.homework.annotation;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data //generates all Getters/Setters, toString(), equals(), hashCode()
+@AllArgsConstructor //generates constructor for all fields in order of their declaration
+@NoArgsConstructor // generates default constructor
+public class Person {
+    private String name;
+
+    private String surname;
+
+    private int age;
+}
